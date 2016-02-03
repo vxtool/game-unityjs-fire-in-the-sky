a075f18db307dfffb735a43c24eb36c26ecf1907
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-02-03 20:45:07 -0200

    update: project name

---
+++ Assets/Scripts/canvas.js
@@ -0,0 +1,10 @@
+﻿import UnityEngine.UI;
+ 
+function Start () {
+ 
+    var canvas = new GameObject ("canvas", Canvas);
+ 
+    canvas.RectTransform.width = 960;
+    canvas.RectTransform.height = 600;
+ 
+}

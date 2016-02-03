e6b832e05c416b7f6e6b4dbe785a587fd38351e2
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-02-03 19:05:04 -0200

    add: unity files

---
+++ Assets/Scripts/mainMenu.js
@@ -0,0 +1,22 @@
+﻿import UnityEngine.SceneManagement;
+#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI () {
+    GUI.Box (Rect (10,10,100,90), "Menu");
+
+    if (GUI.Button (Rect (20,40,80,20), "Play")) {
+        SceneManager.LoadScene('game');
+    }
+
+    if(GUI.Button(new Rect(20,70,80,20), "Tutorial")) {
+        SceneManager.LoadScene('tutorial');
+    }
+}

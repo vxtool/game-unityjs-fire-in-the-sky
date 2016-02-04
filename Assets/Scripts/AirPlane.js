7fa213ac12480960fdc1b0ed863d4f1389bc9d7f
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-02-04 02:05:15 -0200

    update: airplane moviments

---
+++ Assets/Scripts/AirPlane.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+public var range : float;
+
+
+function Update () {
+    var h : float = Input.GetAxis("Horizontal");
+    var v : float = Input.GetAxis("Vertical");
+    var xPos : float = h * range;
+    var yPos : float = v * range;
+    var xCurrent : float = transform.position.x + xPos;
+    var yCurrent : float = transform.position.y + yPos;
+    transform.position = new Vector3(xCurrent, yCurrent, 0);
+}

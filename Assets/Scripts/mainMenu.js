b2612760a9581202eb315c5346fd6a81620c4150
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-02-03 23:42:20 -0200

    update: main menu screen

--- Assets/Scripts/mainMenu.js
+++ Assets/Scripts/mainMenu.js
@@ -1,4 +1,5 @@
 ﻿import UnityEngine.SceneManagement;
+import UnityEngine.Screen;
 #pragma strict
 
 function Start () {
@@ -10,13 +11,16 @@ function Update () {
 }
 
 function OnGUI () {
-    GUI.Box (Rect (10,10,100,90), "Menu");
+	var wButton = 200;
+	var hButton = 60;
+	var xElement =  (width-wButton)/2;
+	var yElement = 400;
 
-    if (GUI.Button (Rect (20,40,80,20), "Play")) {
+    if (GUI.Button (Rect (xElement, yElement, wButton, hButton), "Play")) {
         SceneManager.LoadScene('game');
     }
 
-    if(GUI.Button(new Rect(20,70,80,20), "Tutorial")) {
+    if(GUI.Button(new Rect(xElement, yElement + hButton + 10, wButton, hButton), "Tutorial")) {
         SceneManager.LoadScene('tutorial');
     }
 }

1cc0dcac2982155b57fa25b346fcf04be1c8e52a
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-02-07 19:52:09 -0200

    update: destroy transform gameObject

---
+++ Assets/Scripts/moveObject.js
@@ -0,0 +1,21 @@
+﻿import UnityEngine.Component;
+
+#pragma strict
+
+public var speed : float;
+private var y : float;
+
+function Start () {
+
+}
+
+function Update () {
+	y = transform.position.y;
+	y += speed;
+	transform.position = new Vector3(transform.position.x, y, transform.position.z);
+
+	Debug.Log(y);
+	if(y <= -100 ) {
+		Destroy (transform.gameObject);
+	}
+}

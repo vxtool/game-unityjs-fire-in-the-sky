9cdaca818df60511fc7d46d67509e527aa987a4d
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-02-07 17:24:56 -0200

    add: audio

---
+++ Assets/Scripts/AirPlaneController.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+public var range 				: float;
+public var AirplaneRigidbody	: Rigidbody2D;
+
+//var rend : Renderer;
+
+function Start () {
+	//rend = gameObject.GetComponent.<Renderer>();
+}
+
+function Update () {
+    var h : float = Input.GetAxis("Horizontal");
+    var v : float = Input.GetAxis("Vertical");
+    var xPos : float = h * range;
+    var yPos : float = v * range;
+    var xCurrent : float = transform.position.x + xPos;
+    var yCurrent : float = transform.position.y + yPos;
+
+    //if (rend.isVisible) {
+    	transform.position = new Vector3(xCurrent, yCurrent, 0);
+    //}
+}
---
+++ Assets/Scripts/mainMenuController.js
@@ -0,0 +1,26 @@
+﻿import UnityEngine.SceneManagement;
+import UnityEngine.Screen;
+#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnGUI () {
+	var wButton = 200;
+	var hButton = 60;
+	var xElement =  (width-wButton)/2;
+	var yElement = 400;
+
+    if (GUI.Button (Rect (xElement, yElement, wButton, hButton), "Play")) {
+        SceneManager.LoadScene('game');
+    }
+
+    if(GUI.Button(new Rect(xElement, yElement + hButton + 10, wButton, hButton), "Tutorial")) {
+        SceneManager.LoadScene('tutorial');
+    }
+}

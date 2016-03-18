7728e480feb40612132cb25f8d2b913356259c8f
Hemerson Vianna <hemerson.lourenco@gmail.com>
2016-03-17 21:10:12 -0300

    add: random range for enemies

--- Assets/Scripts/EnemyMoviment.js
+++ Assets/Scripts/EnemyMoviment.js
@@ -1,14 +1,17 @@
-﻿import UnityEngine.Component;
+import UnityEngine.Component;
 
 #pragma strict
 
-var speed 	: float;
+var startX : float;
+var startY : float;
+var endY   = -12;
+var speed : float;
 
 private var y 		: float;
 private var rend 	: Renderer;
 
 function Start () {
-
+  transform.position.y = startY;
 }
 
 function Update () {
@@ -16,9 +19,14 @@ function Update () {
 	y += speed;
 	transform.position = new Vector3(transform.position.x, y, transform.position.z);
 
-	if(rend && !rend.isVisible){
-		Destroy (transform.gameObject);
-	}
+  if(transform.position.y <= endY){
+    transform.position.y = startY;
+    transform.position.x = Random.Range(-13,13);
+   }
+
+	// if(rend && !rend.isVisible){
+	// 	Destroy (transform.gameObject);
+	// }
 }
 
 function OnBecameInvisible () {
@@ -27,4 +35,16 @@ function OnBecameInvisible () {
 
 function OnBecameVisible() {
 	//Debug.Log("visible");
+}
+
+function OnTriggerEnter(other:Collider){
+  if(other.CompareTag("Player")){
+    transform.position.y = startY;
+    transform.position.x = Random.Range(-13,13);
+  }
+
+  if(other.CompareTag("Shot")){
+    transform.position.y = startY;
+    transform.position.x = Random.Range(-13,13);
+  }
 }
